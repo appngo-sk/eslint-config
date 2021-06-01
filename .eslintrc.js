3e8897fdf67dc54919cdd4575d72a1f66e1cb596
roman.strazanec <strazanec.roman@gmail.com>
2021-06-01 22:22:19 +0200

    Edit eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,33 @@
 module.exports = {
-
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint",
+    ],
+    rules: {
+        indent: [
+            "error",
+            4,
+        ],
+        "linebreak-style": [
+            "error",
+            "unix",
+        ],
+        quotes: [
+            "error",
+            "double",
+        ],
+        semi: [
+            "error",
+            "never",
+        ],
+        "no-extra-parens": "all"
+    }
 };
