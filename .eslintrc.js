087052e05bd97ea06be131062c5ab3c5bdf9fc81
roman.strazanec <strazanec.roman@gmail.com>
2021-06-02 11:20:02 +0200

    1.0.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
     ],
     parser: "@typescript-eslint/parser",
     plugins: [
@@ -30,7 +31,7 @@ module.exports = {
         ],
         "no-extra-parens": [
             "error",
-            "all"
+            "all",
         ],
     },
 };
